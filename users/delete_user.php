89c46740fac1a93a69bd8b2438aac36829a3e0a4
danyloosypov <48645561+danyloosypov@users.noreply.github.com>
2023-04-17 19:31:43 +0300

    deleting user with modal window implemented

--- users/delete_user.php
+++ users/delete_user.php
@@ -2,5 +2,39 @@
 
 require_once("../db/dbconnection.php");
 
+$userId = $_POST['user_id'];
+
+if (empty($userId)) {
+  $response = array(
+    'status' => false,
+    'error' => array(
+      'code' => 101,
+      'message' => 'User ID is required.'
+    )
+  );
+  echo json_encode($response);
+} else {
+    $sql = "delete from users WHERE user_id = $userId";
+
+    $res = mysqli_query($connection, $sql);
+ 
+    if($res) {
+        $response = array(
+            'status' => true,
+            'error' => null
+        );
+
+        echo json_encode($response);
+    } else {
+        $response = array(
+            'status' => false,
+            'error' => array(
+              'code' => 102,
+              'message' => 'Error deleting user. Please try again.'
+            )
+          );
+        echo json_encode($response);
+    }
+}
 
 ?>
